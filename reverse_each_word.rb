474520ab8190935077505aad113fee3481964d83
Matt Green <magreen118@gmail.com>
2020-06-06 22:57:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_array = array.each{|element| element.reverse}
-  return new_array
+  puts new_array
+  eturn new_array
 end
 
 reverse_each_word("Hello there, and how are you?")
