c323cc54f3e59214cb8691b6aa76c9aa0864f4ea
Matt Green <magreen118@gmail.com>
2020-06-06 23:11:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,8 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.each{|element| new_array.push(element.reverse)}
-  return new_array
+  reversed_string = new_array.join(" ")
+  return reversed_string
 end
 
 reverse_each_word("Hello there, and how are you?")
