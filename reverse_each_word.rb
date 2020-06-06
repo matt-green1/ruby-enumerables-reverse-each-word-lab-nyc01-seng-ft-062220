d6e321dd4bcae13c4fc8214e097011be0271aa2b
Matt Green <magreen118@gmail.com>
2020-06-06 23:00:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@
 
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = array.each{|element| element.reverse}
+  new_array = []
+  array.each{|element| new_array.push(element.reverse)}
   puts new_array[0]
   return new_array
 end
